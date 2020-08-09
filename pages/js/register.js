9163c4961d745c45c1ce1b6bec84510f9d20722f
agbeyeye <dnewlife0@gmail.com>
2020-08-08 20:25:28 -1200

    Added submit-vehicle and  register file

---
+++ pages/js/register.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+	$("#register").click(function(){
+		var form = $("#registration-form");
+		form.validate({
+			rules: {
+				email: {required: true,},
+				username:{required:true, minlength:6,},
+				password:{required: true,minlength:8,},
+				confirmPassword:{required:true,equalTo:'#password',},
+				policy:{required:true,},
+				
+			},
+			messages: {
+				email: {required: "email is required",},
+				username:{required:"this field is required",},
+				password:{required: "password is required",},
+				confirmPassword:{required:"this field is required",equalTo:"password does not match",},
+				policy:{required:"You must accept in order to register",},				
+			}
+		});
+
+		if(form.valid()==true){
+			form.submit();
+		}
+	});
+});
